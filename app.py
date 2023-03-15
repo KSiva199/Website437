53c3eafe6972beaf367dec79ac8b3880ebb947f2
KSiva199 <114784297+KSiva199@users.noreply.github.com>
2023-03-15 13:23:32 -0400

    home.html changes

--- app.py
+++ app.py
@@ -19,37 +19,8 @@ sess.init_app(app)
 #Basic root route - show the word 'homepage'
 @app.route('/')  #route name
 def home(): #view function
-    return 'homepage'
+    return render_template('home.html')
 
-    
-@app.route('/confirm',methods=['GET','POST'])
-def confirm():
-    #product = request.form.get('product')
-    product = session['product']
-    ship = request.form.get('ship')
-    return render_template('confirm.html',product=product,ship=ship)
-
-#test setting a session:
-@app.route('/set')
-def set():
-    session['key'] = 'value'
-    return 'ok'
-
-#test getting a session:
-@app.route('/get')
-def get():
-    return session.get('key', 'not set')
-
-@app.route('/test')
-def test():
-    user = 'Tommy'
-    return render_template('test.html',username = user)
-
-@app.route('/list_users')
-def list_users():
-    o = Users()
-    o.getAll()
-    return render_template('list_users.html',objs = o)
 
 @app.route('/users/manage',methods=['GET','POST'])
 def manage_user():
@@ -88,30 +59,6 @@ def manage_user():
         o.getById(pkval)
         return render_template('users/manage.html',obj = o)
 
-#display form   
-@app.route('/enterName')
-def enterName():
-    return render_template('nameForm.html')
-
-#process form   
-@app.route('/submitName',methods=['GET','POST'])
-def submitName():
-    username = request.form.get('myname')
-    othername = request.form.get('othername')
-    print(othername)
-    print(username)
-    #At this point we would INSERT the user's name to the mysql table
-    return render_template('message.html',msg='name '+str(username)+' added!')
-
-@app.route('/elements')
-def elements():
-    return render_template('formelements.html')
-
-# endpoint route for static files
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
-      
   
 if __name__ == '__main__':
    app.run(host='127.0.0.1',debug=True)  
